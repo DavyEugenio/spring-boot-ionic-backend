487cca498089964a9d1fe6a7bba32d51a61926dc
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-01-07 02:50:56 -0300

    Teste - Primeira classe de dominio - 'Categoria'

--- src/main/java/com/deno/cursomc/resourses/CategoriaResourses.java
+++ src/main/java/com/deno/cursomc/resourses/CategoriaResourses.java
@@ -1,15 +1,24 @@
 package com.deno.cursomc.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.deno.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourses {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String teste() {
-		return "REST funoncia!!!!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new  Categoria(1, "informática");
+		Categoria cat2 = new  Categoria(1, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
