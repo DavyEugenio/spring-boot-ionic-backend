1dd3f57c125c122d1e63c954b9c4860818f9b031
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-01-08 19:08:04 -0300

    Estado e Cidade

---
+++ src/main/java/com/deno/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.deno.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.deno.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
