96babe66b5d048190ad449c5e8a62d0b8c48221d
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-02-29 17:20:41 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/deno/cursomc/resourses/exception/ResourseExceptionHandler.java
+++ src/main/java/com/deno/cursomc/resourses/exception/ResourseExceptionHandler.java
@@ -7,13 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.deno.cursomc.services.exceptions.DataIntegrityException;
 import com.deno.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourseExceptionHandler {
+	
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e,	HttpServletRequest request) {
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e,
+			HttpServletRequest request) {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e,
+			HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/deno/cursomc/services/CategoriaService.java
+++ src/main/java/com/deno/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.deno.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.deno.cursomc.domain.Categoria;
 import com.deno.cursomc.repositories.CategoriaRepository;
+import com.deno.cursomc.services.exceptions.DataIntegrityException;
 import com.deno.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,9 +27,18 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!");
+		}
 	}
 }
