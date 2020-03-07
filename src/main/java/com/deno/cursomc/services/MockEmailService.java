7991d703fd7466432fb1561dfe8b16f9b747d87f
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-03-07 08:31:10 -0300

    Email HTML

--- src/main/java/com/deno/cursomc/services/MockEmailService.java
+++ src/main/java/com/deno/cursomc/services/MockEmailService.java
@@ -3,6 +3,8 @@ package com.deno.cursomc.services;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 
 public class MockEmailService extends AbstractEmailService {
@@ -12,6 +14,13 @@ public class MockEmailService extends AbstractEmailService {
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado!");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado!");
 	}
