cd1a21a114ff6ac92f1e2721d70d53d4ad7eeae4
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-03-15 01:40:52 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/deno/cursomc/domain/Cliente.java
+++ src/main/java/com/deno/cursomc/domain/Cliente.java
@@ -46,6 +46,7 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
+	private String imageUrl;
 
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -165,5 +166,13 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 }
